5376c74f70f9fa09a1482cbc3c007175aa1e1ca0
Ivan 'w23' Avdeev <me@w23.ru>
2015-11-28 18:34:33 +0600

    added ace text editor

--- D3.js
+++ D3.js
@@ -15,7 +15,9 @@ D3.createContextOnCanvas = function (canvas) {
 	var state = {
 		buffers: {},
 		program: null,
-		attributes: {}
+		attributes: {},
+		textures: [],
+		active_texture: 0
 	};
 
 	if (!gl) {
@@ -43,9 +45,6 @@ D3.createContextOnCanvas = function (canvas) {
 		TriangleStrip: gl.TRIANGLE_STRIP,
 		TriangleFan: gl.TRIANGLE_FAN
 	}
-
-	state.viewport = [0, 0, canvas.width, canvas.height];
-	gl.viewport(0, 0, canvas.width, canvas.height);
 
 	var makeShader = function (source, type) {
 		var shader = gl.createShader(type);
@@ -102,22 +101,182 @@ D3.createContextOnCanvas = function (canvas) {
 		}
 
 		return buffer;
-	}
+	};
 
 	context.createVertexBuffer = function () {
 		return createBuffer(gl.ARRAY_BUFFER);
-	}
+	};
 
 	context.createIndexBuffer = function () {
 		return createBuffer(gl.ELEMENT_ARRAY_BUFFER);
+	};
+
+	context.TextureFormat = {
+		R_8: {
+			format: gl.LUMINANCE,
+			type: gl.UNSIGNED_BYTE
+		},
+		RA_8: {
+			format: gl.LUMINANCE_ALPHA,
+			type: gl.UNSIGNED_BYTE
+		},
+		RGB_8: {
+			format: gl.RGB,
+			type: gl.UNSIGNED_BYTE
+		},
+		RGBA_8: {
+			format: gl.RGBA,
+			type: gl.UNSIGNED_BYTE
+		},
+		RGB_565: {
+			format: gl.RGB,
+			type: gl.UNSIGNED_SHORT_5_6_5
+		},
+		RGBA_5551: {
+			format: gl.RGBA,
+			type: gl.UNSIGNED_SHORT_5_5_5_1
+		},
+		RGBA_4444: {
+			format: gl.RGBA,
+			type: gl.UNSIGNED_SHORT_4_4_4_4
+		}
+	};
+
+	context.createTexture = function () {
+		var name = gl.createTexture();
+		var tex = {};
+		var width = 0, height = 0;
+
+		tex.getWidth = function () {
+			return width;
+		}
+
+		tex.getHeight = function () {
+			return height;
+		}
+
+		tex.getName = function () {
+			return name;
+		}
+
+		tex.bind = function () {
+			if (state.textures[state.active_texture] !== this) {
+				state.textures[state.active_texture] = this;
+				gl.bindTexture(gl.TEXTURE_2D, name);
+			}
+			return this;
+		}
+
+		tex.bind.apply(this);
+
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.LINEAR);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.LINEAR);
+
+		tex.uploadEmpty = function (format, width_in, height_in) {
+			this.bind();
+			width = width_in;
+			height = height_in;
+			gl.texImage2D(gl.TEXTURE_2D, 0, format.format,
+				width, height, 0, format.type, null);
+			return this;
+		}
+
+		tex.uploadImage = function (image) {
+			this.bind();
+			gl.texImage2D(gl.TEXTURE_2D, 0, format.format, format.format, format.type, image);
+			width = image.naturalWidth;
+			height = image.naturalHeight;
+			return this;
+		}
+
+		tex.generateMipmap = function () {
+			this.bind();
+			gl.generateMipmap(gl.TEXTURE_2D);
+			return this;
+		}
+
+		return tex;
+	};
+
+	context.createFramebuffer = function () {
+		var name = gl.createFramebuffer();
+		var that = {};
+
+		var texture = null;
+		var viewport = null;
+		var depth = null;
+
+		that.attachColor = function (texture_in) {
+			if (texture) { throw D3.Exception('Invalid argument'); }
+			texture = texture_in;
+
+			gl.bindFramebuffer(gl.FRAMEBUFFER, name);
+			gl.framebufferTexture2D(gl.FRAMEBUFFER,
+				gl.COLOR_ATTACHMENT0, gl.TEXTURE2D, texture.getName(), 0);
+
+			if (!viewport) {
+				viewport = [0, 0, texture.getWidth(), texture.getHeight()];
+			}
+
+			return this;
+		}
+
+		that.attachDepth = function () {
+			depth = {
+				name: gl.createRenderbuffer(),
+				width: 0,
+				height: 0
+			};
+			return this;
+		}
+
+		that.specifyViewport = function (x, y, w, h) {
+			viewport = [x, y, w, h];
+			return this;
+		}
+
+		that.bind = function () {
+			if (state.framebuffer !== this) {
+				gl.bindFramebuffer(gl.FRAMEBUFFER, name);
+				state.framebuffer = this;
+			}
+		}
+
+		that.use = function () {
+			this.bind();
+
+			if (depth) {
+				if (depth.width != texture.getWidth() || depth.height != texture.getHeight()) {
+					gl.bindRenderbuffer(gl.RENDERBUFFER);
+					gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16,
+						texture.getWidth(), texture.getHeight());
+					depth.width = texture.getWidth();
+					depth.height = texture.getHeight();
+					gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER);
+				}
+			}
+
+			gl.viewport.apply(gl, viewport);
+		}
+
+		return that;
 	}
 
 	context.setDestination = function (destdesc) {
-		throw 'Not implemented';
+		var viewport = des
+		if (!destdesc) {
+			destdesc = {}
+			destdesc.viewport = [0, 0, canvas.width, canvas.height];
+		}
+
+		//if ()
+		gl.viewport(0, 0, canvas.width, canvas.height);
 	}
 	
 	context.setWrite = function (writedesc) {
-		throw 'Not implemented';
+		throw D3.Exception('Not implemented');
 	}
 
 	context.fill = function (filldesc) {
@@ -171,7 +330,13 @@ D3.createContextOnCanvas = function (canvas) {
 		state.attributes[index] = true;
 	}
 
-	context.paint = function (paintdesc) {
+	context.paint = function (paintdesc, write, framebuffer) {
+		if (framebuffer) {
+			framebuffer.use();
+		} else {
+
+		}
+
 		if (state.program !== paintdesc.program) {
 			state.program = paintdesc.program;
 			gl.useProgram(state.program);
--- test.js
+++ test.js
@@ -1,42 +1,67 @@
 function start() {
+	var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/solarized_light");
+    editor.getSession().setMode("ace/mode/glsl");
+    /*editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: false
+    });*/
+
 	try {
-	var ctx = D3.createContextOnCanvas(document.getElementById('glcanvas'));
+		var ctx = D3.createContextOnCanvas(document.getElementById('glcanvas'));
 
-	var buffer = ctx.createVertexBuffer().upload(new Float32Array([1,-1,1,1,-1,-1,-1,1]));
+		var buffer = ctx.createVertexBuffer().upload(new Float32Array([1,-1,1,1,-1,-1,-1,1]));
 
-	var program = ctx.createProgram({
-		vertex: 'attribute vec2 av2_vtx;varying vec2 vv2_v;void main(){vv2_v = av2_vtx;gl_Position = vec4(av2_vtx, 0., 1.);}',
-		fragment: 'precision mediump float;varying vec2 vv2_v;uniform float uf_time;void main(){gl_FragColor=vec4(vv2_v,sin(uf_time),1.);}'
-	});
+		var vertex_source = 'attribute vec2 av2_vtx;varying vec2 vv2_v;void main(){vv2_v = av2_vtx;gl_Position = vec4(av2_vtx, 0., 1.);}';
 
-	var starttime = Date.now();
-
-	paint = function paint() {
-		requestAnimationFrame(paint);
-
-		var time = (Date.now() - starttime) * 0.001;
-
-		ctx.fill({color: [Math.sin(time), 0, 0, 1]});
-
-		ctx.paint({
-			program: program,
-			attributes: {
-				'av2_vtx': {
-					buffer: buffer,
-					size: 2,
-					type: ctx.AttribType.Float,
-					offset: 0
-				}
-			},
-			uniforms: {
-				'uf_time': time
-			},
-			mode: ctx.Primitive.TriangleStrip,
-			count: 4
+		var program = ctx.createProgram({
+			vertex: vertex_source,
+			fragment: editor.getValue()
 		});
-	}
 
-	paint();
+		editor.getSession().on('change', function(e) {
+			try {
+				program = ctx.createProgram({
+					vertex: vertex_source,
+					fragment: editor.getValue()
+				});
+			} catch (except) {
+				console.log(except);
+			}
+		});
+
+		var starttime = Date.now();
+
+		resize = function () {
+		}
+
+		paint = function paint() {
+			requestAnimationFrame(paint);
+
+			var time = (Date.now() - starttime) * 0.001;
+
+			ctx.fill({color: [Math.sin(time), 0, 0, 1]});
+
+			ctx.paint({
+				program: program,
+				attributes: {
+					'av2_vtx': {
+						buffer: buffer,
+						size: 2,
+						type: ctx.AttribType.Float,
+						offset: 0
+					}
+				},
+				uniforms: {
+					'uf_time': time
+				},
+				mode: ctx.Primitive.TriangleStrip,
+				count: 4
+			});
+		}
+
+		paint();
 	} catch(e) {
 		console.log(e);
 	}
